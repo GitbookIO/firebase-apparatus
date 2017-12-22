eaee47371dee15eb267929b7fdd893fb3d0b01fc
Johan Preynat <johan.preynat@gmail.com>
2017-12-22 11:04:48 +0100

    Add transformHashOptions helper for auth import

--- src/auth/types.js
+++ src/auth/types.js
@@ -62,3 +62,36 @@ export type GoogleUser = {
     customAttributes?: string,
     providerUserInfo: GoogleProviderUserInfo[]
 };
+
+// Options for the auth:import hash algorithm
+export type HashOptions = {
+    // Hash algorithm used in password for these accounts
+    hashAlgo?: string,
+    // Key used in hash algorithm
+    hashKey?: string,
+    // Salt separator which will be appended to salt when verifying password. only used by SCRYPT now
+    saltSeparator?: string,
+    // Number of rounds for hash calculation
+    rounds?: number,
+    // Memory cost for firebase scrypt, or cpu/memory cost for standard scrypt
+    memCost?: number,
+    // Parallelization for standard scrypt
+    parallelization?: number,
+    // Block size (normally is 8) for standard scrypt
+    blockSize?: number,
+    // Derived key length for standard scrypt
+    dkLen?: number
+};
+
+// Hash options for the Google API
+export type GoogleHashOptions = {
+    hashAlgorithm?: string,
+    signerKey?: string,
+    saltSeparator?: string,
+    rounds?: number,
+    memoryCost?: number,
+    cpuMemCost?: number,
+    parallelization?: number,
+    blockSize?: number,
+    dkLen?: number
+};
